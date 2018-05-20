7d92b92339290b819bea2c5ea5e618a214909038
German Topalov <ru00161131@gmail.com>
2018-05-20 20:19:21 +0700

    [upd] added validations to auth and register forms

--- components/templates/Registration/Registration.js
+++ components/templates/Registration/Registration.js
@@ -5,26 +5,72 @@ import A_H from 'widgets/A_H/A_H'
 import A_Btn from 'widgets/A_Btn/A_Btn'
 import './Registration.scss'
 import {cssClassName} from 'utils'
+import {isEmail, required, equalToField} from "../../../utils/validateHelpers";
 const cn = cssClassName('Registration')
 
 class Registration extends Component {
 
   state = {
-    user: '',
-    password: '',
-    passwordConfirm: '',
+    form: {
+      email: '',
+      password: '',
+      passwordConfirm: ''
+    },
+    validations: {
+      email: [required, isEmail],
+      password: [required],
+      passwordConfirm: [required, equalToField('password')]
+    },
+    formValid: false,
+    errors: {}
+  }
+
+  validateFields = (formValues, onFinish) => {
+    const {form, validations} = this.state
+    let errors = {}
+
+    Object.keys(formValues).forEach(fieldName => {
+      if(validations[fieldName]) {
+        validations[fieldName].forEach(validation => {
+          if(errors[fieldName]) return
+          errors[fieldName] = validation(formValues[fieldName], form)
+        })
+      }
+    })
+
+    const hasErrors = Object.values(errors).filter(x => !!x).length > 0
+
+    this.setState({errors, formValid: !hasErrors}, () => onFinish())
+  }
+
+  handleChange = (fieldValue, fieldName) => {
+    const { form, errors } = this.state
+
+    this.setState({
+      form: {
+        ...form,
+        [fieldName]: fieldValue
+      },
+      errors: {
+        ...errors,
+        [fieldName]: undefined
+      }
+    })
   }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    const {user, password, passwordConfirm} = this.state
-    console.log('submit', user, password, passwordConfirm)
+    const {form} = this.state
+
+    this.validateFields(form, () => {
+      this.state.formValid && console.log('submit values', form.email, form.password, form.passwordConfirm)
+    })
   }
 
   render() {
 
 
-    const {} = this.state
+    const { email, password, passwordConfirm, errors } = this.state
 
     return (
       <div className={cn()}>
@@ -33,20 +79,26 @@ class Registration extends Component {
         <form className={cn('form')} onSubmit={this.handleSubmit}>
           <A_InputText
             mix={cn('input')}
+            value={email}
             placeholder='E-MAIL'
-            handleChange={user => this.setState({user})}
+            handleChange={value => this.handleChange(value, 'email')}
+            error = {errors.email}
           />
           <A_InputText
             mix={cn('input')}
+            value={password}
             type='password'
             placeholder='PASSWORD'
-            handleChange={password => this.setState({password})}
+            handleChange={value => this.handleChange(value, 'password')}
+            error = {errors.password}
           />
           <A_InputText
             mix={cn('input')}
+            value={passwordConfirm}
             type='password'
             placeholder='RE-ENTER PASSWORD'
-            handleChange={passwordConfirm => this.setState({passwordConfirm})}
+            handleChange={value => this.handleChange(value, 'passwordConfirm')}
+            error = {errors.passwordConfirm}
           />
           <A_Btn
             mix={cn('submit-btn')}
