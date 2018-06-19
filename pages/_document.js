c9fe899a7ab7c008e0b3a50e4617a5d27addd2fc
Kherel Kechil <kherel@gmail.com>
2018-06-19 09:56:31 +0900

    [fix] http/css browser console errors fix

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
       <html>
       <Head>
         <link rel="stylesheet" href="/_next/static/style.css" />
+        <link rel="icon" href="data:;base64,iVBORw0KGgo=" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </Head>
       <body >
